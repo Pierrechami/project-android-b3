f67e3868358e28c0c4ffc4ad294e9cebd0a87fb2
Quentin Launay <50755766+Elsword24@users.noreply.github.com>
2023-02-23 17:24:21 +0100

    upt layer and ass categories & product

--- app/src/main/java/fr/epsi/myapplication/BaseActivity.kt
+++ app/src/main/java/fr/epsi/myapplication/BaseActivity.kt
@@ -1,16 +1,12 @@
 package fr.epsi.myapplication
 
-import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 open class BaseActivity : AppCompatActivity(){
 
     fun setHeaderTitle(title: String?) {
-        val textView = findViewById<TextView>(R.id.textViewTitle)
+        val textView = findViewById<TextView>(R.id.main_header)
         textView.setText(title)
     }
 
