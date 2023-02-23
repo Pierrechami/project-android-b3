3b5aefc831bb3e88762cf07556a30b861db672f9
Quentin Launay <50755766+Elsword24@users.noreply.github.com>
2023-02-23 16:37:52 +0100

    add all activities

---
+++ app/src/main/java/fr/epsi/myapplication/ProductDetails.kt
@@ -0,0 +1,11 @@
+package fr.epsi.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+    }
+}
---
+++ app/src/main/java/fr/epsi/myapplication/StudentInfo.kt
@@ -0,0 +1,11 @@
+package fr.epsi.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_info)
+    }
+}
