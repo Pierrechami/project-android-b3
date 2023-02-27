7551d5b727ee50f47b2e9a4e62f7b736fe004bad
Quentin Launay <50755766+Elsword24@users.noreply.github.com>
2023-02-27 19:26:46 +0100

    upt all

--- app/src/main/java/fr/epsi/myapplication/CategorieActivity.kt
+++ app/src/main/java/fr/epsi/myapplication/CategorieActivity.kt
@@ -1,12 +1,11 @@
 package fr.epsi.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class CategorieActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_categorie)
+        setContentView(R.layout.activity_categories)
         setHeaderTitle("Rayons")
         showBack()
     }
