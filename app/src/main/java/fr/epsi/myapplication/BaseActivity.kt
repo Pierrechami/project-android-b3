eb16d1f4ef96fafc113756969c7d874bd8232547
Julien Flusin <julienflusin3@gmail.com>
2023-02-27 01:20:46 +0100

    Create baseActivity methods

--- app/src/main/java/fr/epsi/myapplication/BaseActivity.kt
+++ app/src/main/java/fr/epsi/myapplication/BaseActivity.kt
@@ -1,13 +1,60 @@
 package fr.epsi.myapplication
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 open class BaseActivity : AppCompatActivity(){
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("EPSI B3 PROJECT ANDROID","################ onCreate :"+javaClass.simpleName)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("EPSI B3 PROJECT ANDROID","################ onStart :"+javaClass.simpleName)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("EPSI B3 PROJECT ANDROID","################ onResume :"+javaClass.simpleName)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("EPSI B3 PROJECT ANDROID","################ onPause :"+javaClass.simpleName)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("EPSI B3 PROJECT ANDROID","################ onStop :"+javaClass.simpleName)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("EPSI B3 PROJECT ANDROID","################ onDestroy :"+javaClass.simpleName)
+    }
+
+    override fun finish() {
+        super.finish()
+        Log.i("EPSI B3 PROJECT ANDROID","################ finish :"+javaClass.simpleName)
+    }
+
     fun setHeaderTitle(title: String?) {
-        val textView = findViewById<TextView>(R.id.main_header)
+        val textView = findViewById<TextView>(R.id.textViewTitle)
         textView.setText(title)
+    }
+
+    fun showBack() {
+        val imageViewBack = findViewById<ImageView>(R.id.imageViewBack)
+        imageViewBack.visibility = View.VISIBLE
+        imageViewBack.setOnClickListener(View.OnClickListener {
+            finish()
+        })
     }
 
 }
